f31a1e9452ccdd1086e46fcd0f00d895d62d65b6
ayoub1brm <105363414+ayoub1brm@users.noreply.github.com>
2024-06-05 00:38:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import subprocess
 from discord_bot.discord_bot import setup_discord_bot
 from database.database import Database
 
+# Get the token from an environment variable
+discord_token = os.getenv('DISCORD_TOKEN')
+
 def run_streamlit():
     os.system('streamlit run app.py')
 
 def run_discord_bot():
-    setup_discord_bot({{DISCORD_TOKEN}})
+    setup_discord_bot(discord_token)
 
 if __name__ == "__main__":
     # Run Streamlit app in a separate thread
