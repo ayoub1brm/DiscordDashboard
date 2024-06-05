d476bd29d0057c389df9450d7efdd9879c5be054
ayoub1brm <105363414+ayoub1brm@users.noreply.github.com>
2024-06-05 13:17:16 +0200

    Update database.py

--- database/database.py
+++ database/database.py
@@ -3,16 +3,27 @@ import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import sqlite3
 from datetime import datetime, timedelta
+from threading import Lock
 
 class Database:
-    def __init__(self, db_path):
+    _instance = None
+    _lock = Lock()
+
+    def __new__(cls, *args, **kwargs):
+        with cls._lock:
+            if cls._instance is None:
+                cls._instance = super().__new__(cls)
+                cls._instance._initialize(*args, **kwargs)
+            return cls._instance
+
+    def _initialize(self, db_path):
         self.db_path = db_path
         self.conn = None
         self.cursor = None
 
     def connect(self):
         if self.conn is None:
-            self.conn = sqlite3.connect(self.db_path, timeout=30)
+            self.conn = sqlite3.connect(self.db_path, timeout=30, check_same_thread=False)
             self.conn.execute('PRAGMA journal_mode=WAL;')
             self.cursor = self.conn.cursor()
 
@@ -24,7 +35,7 @@ class Database:
             self.cursor = None
 
     def execute(self, query, params=(), retries=5):
-        for _ in range(retries):
+        for i in range(retries):
             try:
                 self.connect()
                 self.cursor.execute(query, params)
@@ -32,7 +43,7 @@ class Database:
                 return
             except sqlite3.OperationalError as e:
                 if 'database is locked' in str(e):
-                    time.sleep(2)  # wait for 2 seconds before retrying
+                    time.sleep(2 ** i)  # Exponential backoff
                 else:
                     raise
             finally:
@@ -40,5 +51,3 @@ class Database:
 
     def create_tables(self):
         raise NotImplementedError("Subclasses should implement this!")
-
-    # Add other common methods if necessary
