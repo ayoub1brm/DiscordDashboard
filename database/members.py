38423d76e416ad83184c13c30e1852d9a074cdd4
ayoub1brm <105363414+ayoub1brm@users.noreply.github.com>
2024-06-05 15:48:03 +0200

    Update members.py

--- database/members.py
+++ database/members.py
@@ -2,7 +2,7 @@ from database.database import Database
 
 class MembersDatabase(Database):
     def create_tables(self):
-        self.cursor.execute('''
+        self.execute('''
             CREATE TABLE IF NOT EXISTS Members (
                 member_id INTEGER,
                 username TEXT,
@@ -15,23 +15,20 @@ class MembersDatabase(Database):
                 FOREIGN KEY (role_id) REFERENCES Roles(role_id)
             )
         ''')
-
-    def close(self):
-        self.conn.close()
         
     def insert_member(self, member_data):
-        self.cursor.execute('''
+        self.execute('''
             INSERT INTO Members (member_id, username, discriminator, join_date, leave_date, is_bot, activity_status, role_id)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?)
         ''', member_data)
 
     def update_member_leave_date(self, member_id, leave_date):
-        self.cursor.execute('''
+        self.execute('''
             UPDATE Members SET leave_date = ? WHERE member_id = ?
         ''', (leave_date, member_id))
 
     def get_total_member_count(self):
-        self.cursor.execute('''
+        self.execute('''
             SELECT COUNT(*) FROM (SELECT * FROM Members GROUP BY member_id) WHERE leave_date is NULL AND is_bot = 0
         ''')
         result = self.cursor.fetchone()[0]
@@ -39,7 +36,7 @@ class MembersDatabase(Database):
         return result
 
     def get_latest_member_joined_at(self):
-        self.cursor.execute('SELECT MAX(join_date) FROM Members')
+        self.execute('SELECT MAX(join_date) FROM Members')
         result = self.cursor.fetchone()[0]
         self.close()
         return result
