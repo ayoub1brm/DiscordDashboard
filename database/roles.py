2b9db19495162f7b4df1074f97d4362df79658ce
ayoub1brm <105363414+ayoub1brm@users.noreply.github.com>
2024-06-05 15:45:47 +0200

    Update roles.py

--- database/roles.py
+++ database/roles.py
@@ -2,32 +2,21 @@ from database.database import Database
 
 class RolesDatabase(Database):
     def create_tables(self):
-        self.cursor.execute('''
+        self.execute('''
             CREATE TABLE IF NOT EXISTS Roles (
                 role_id INTEGER,
                 role_name TEXT
             )
         ''')
 
-    def close(self):
-        self.conn.close()
-
     def insert_role(self, role_data):
-        self.cursor.execute('''
+        self.execute('''
             INSERT INTO Roles (role_id, role_name)
             VALUES (?, ?)
         ''', role_data)
 
-    def get_role_distribution(self):
-        self.cursor.execute('''
-            SELECT Roles.role_name, COUNT(Members.member_id) 
-            FROM Members 
-            JOIN Roles ON Members.role_id = Roles.role_id
-            WHERE Members.is_bot = 0
-            GROUP BY Roles.role_name
-        ''')
-        result = self.cursor.fetchall()
-        self.close()
-        return result
-
-    # Add other Roles specific methods
+    def insert_roles(self, roles_data):
+        self.executemany('''
+            INSERT INTO Roles (role_id, role_name)
+            VALUES (?, ?)
+        ''', roles_data)
