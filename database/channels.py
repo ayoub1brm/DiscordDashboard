05a4551994669d89fa320c244456b788fd2d1be9
ayoub1brm <105363414+ayoub1brm@users.noreply.github.com>
2024-06-05 12:38:24 +0200

    Create channels.py

---
+++ database/channels.py
@@ -0,0 +1,25 @@
+from database import Database
+
+class ChannelsDatabase(Database):
+    def create_tables(self):
+        self.execute('''
+            CREATE TABLE IF NOT EXISTS Channels (
+                channel_id INTEGER PRIMARY KEY,
+                channel_name TEXT
+            )
+        ''')
+
+    def insert_channel(self, channel_id, channel_name):
+        self.execute('''
+            INSERT OR IGNORE INTO Channels (channel_id, channel_name)
+            VALUES (?, ?)
+        ''', (channel_id, channel_name))
+
+    def get_channels(self):
+        self.connect()
+        self.cursor.execute('SELECT channel_id, channel_name FROM Channels')
+        result = self.cursor.fetchall()
+        self.close()
+        return result
+
+    # Add other Channels specific methods
